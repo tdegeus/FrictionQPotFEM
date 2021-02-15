2ce57187139e7fcdf809f275df57d2e9eb87887b
Tom de Geus <tom@geus.me>
2021-02-15 11:41:30 +0100

    [CI] Turning off xsimd on Linux

--- include/FrictionQPotFEM/UniformSingleLayer2d.hpp
+++ include/FrictionQPotFEM/UniformSingleLayer2d.hpp
@@ -1255,10 +1255,10 @@ inline void LocalTriggerFineLayerFull::setState(
                 }
             }
 
-            auto idx = xt::argmin(W)();
-            m_smin(e, q) = S.data()[idx];
-            m_pmin(e, q) = P.data()[idx];
-            m_Wmin(e, q) = W.data()[idx];
+            auto idx = xt::argmin(W);
+            m_smin(e, q) = S[idx];
+            m_pmin(e, q) = P[idx];
+            m_Wmin(e, q) = W[idx];
         }
     }
 }
