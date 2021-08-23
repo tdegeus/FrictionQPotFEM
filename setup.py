5257b00e91cfb1b4ed6f0ef156ac40677a8facf7
Tom de Geus <tom@geus.me>
2021-08-23 15:04:59 +0200

    [setup.py] Improve verbosity

--- setup.py
+++ setup.py
@@ -105,9 +105,12 @@ class CMakeBuild(build_ext):
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
 
+        print(["cmake", ext.sourcedir] + cmake_args)
         subprocess.check_call(
             ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
         )
+
+        print(["cmake", "--build", "."] + build_args)
         subprocess.check_call(
             ["cmake", "--build", "."] + build_args, cwd=self.build_temp
         )
