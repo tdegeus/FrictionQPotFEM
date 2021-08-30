5b015326e2e09b9862b1caabcb91e19f1898a835
Tom de Geus <tom@geus.me>
2021-08-30 16:35:15 +0200

    Minor bugfixes

--- include/FrictionQPotFEM/version.h
+++ include/FrictionQPotFEM/version.h
@@ -31,7 +31,7 @@ Tip: use the environment variable ``SETUPTOOLS_SCM_PRETEND_VERSION``
 to overwrite the automatic version.
 */
 #ifndef FRICTIONQPOTFEM_VERSION
-#define FRICTIONQPOTFEM_VERSION "@FrictionQPotFEM_VERSION@"
+#define FRICTIONQPOTFEM_VERSION "@PROJECT_VERSION@"
 #endif
 
 namespace FrictionQPotFEM {
