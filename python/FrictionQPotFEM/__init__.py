e219df4f9b63bf6596a217629d8be31b407fe821
Tom de Geus <tom@geus.me>
2021-09-16 17:22:31 +0200

    Removing unnecessary imports

--- python/FrictionQPotFEM/__init__.py
+++ python/FrictionQPotFEM/__init__.py
@@ -1,5 +1,1 @@
-from . import Generic2d  # noqa: F401
-from . import UniformMultiLayerIndividualDrive2d  # noqa: F401
-from . import UniformMultiLayerLeverDrive2d  # noqa: F401
-from . import UniformSingleLayer2d  # noqa: F401
 from ._FrictionQPotFEM import *  # noqa: F401, F403
