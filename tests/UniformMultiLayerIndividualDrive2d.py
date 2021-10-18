1c0503e55ee4c9fae8d0174b621241f8d2eff9ce
Tom de Geus <tom@geus.me>
2021-10-18 13:16:05 +0200

    Adding tests

--- tests/UniformMultiLayerIndividualDrive2d.py
+++ tests/UniformMultiLayerIndividualDrive2d.py
@@ -11,56 +11,24 @@ class test_Generic2d(unittest.TestCase):
     Tests
     """
 
-    def test_scalePerturbation(self):
-
-        mesh = GooseFEM.Mesh.Quad4.Regular(3, 3)
-        nelem = mesh.nelem()
-        system = FrictionQPotFEM.Generic2d.HybridSystem(
-            mesh.coor(),
-            mesh.conn(),
-            mesh.dofs(),
-            np.arange(mesh.nnode() * mesh.ndim()),
-            [0, 1, 2, 6, 7, 8],
-            [3, 4, 5],
-        )
-
-        system.setMassMatrix(np.ones(nelem))
-        system.setDampingMatrix(np.ones(nelem))
-        system.setElastic(np.ones(6), np.ones(6))
-        system.setPlastic(np.ones(3), np.ones(3), [[100.0], [100.0], [100.0]])
-        system.setDt(1.0)
-
-        x = mesh.coor()
-        delta_u = np.zeros_like(x)
-
-        for i in range(delta_u.shape[0]):
-            delta_u[i, 0] = 0.1 * (x[i, 0] - x[0, 0])
-
-        system.setU(delta_u)
-        Epsd_delta = GMat.Deviatoric(system.plastic_Eps(0, 0))
-        system.setU(np.zeros_like(x))
-
-        for eps_target in [0.001, 0.1, 1.0]:
-            Epsd_t = GMat.Deviatoric(system.plastic_Eps(0, 0))
-            c = FrictionQPotFEM.Generic2d.scalePerturbation(Epsd_t, Epsd_delta, eps_target)
-            system.setU(system.u() + c * delta_u)
-            self.assertTrue(np.isclose(GMat.Epsd(system.plastic_Eps(0, 0)), eps_target))
-
     def test_eventDrivenSimpleShear(self):
 
-        mesh = GooseFEM.Mesh.Quad4.Regular(3, 3)
+        mesh = GooseFEM.Mesh.Quad4.Regular(nx=3, ny=2 * 3 + 1)
         nelem = mesh.nelem()
+        conn = mesh.conn()
         elem = mesh.elementgrid()
         dofs = mesh.dofs()
-        dofs[mesh.nodesLeftOpenEdge(), ...] = dofs[mesh.nodesRightOpenEdge(), ...]
+        dofs[mesh.nodesLeftEdge()[1:], ...] = dofs[mesh.nodesRightEdge()[1:], ...]
+        layers = [elem[:3, :].ravel(), elem[3, :].ravel(), elem[4:, :].ravel()]
 
-        system = FrictionQPotFEM.Generic2d.HybridSystem(
+        system = FrictionQPotFEM.UniformMultiLayerIndividualDrive2d.System(
             mesh.coor(),
             mesh.conn(),
             dofs,
-            dofs[np.concatenate((mesh.nodesBottomEdge(), mesh.nodesTopEdge())), :].ravel(),
-            [0, 1, 2, 6, 7, 8],
-            [3, 4, 5],
+            dofs[mesh.nodesBottomEdge(), :].ravel(),
+            layers,
+            [np.unique(conn[i, :]) for i in layers],
+            [False, True, False],
         )
 
         nelas = system.elastic().size
@@ -73,14 +41,15 @@ class test_Generic2d(unittest.TestCase):
         system.setElastic(np.ones(nelas), np.ones(nelas))
         system.setPlastic(np.ones(nplas), np.ones(nplas), epsy)
         system.setDt(1.0)
+        system.layerSetDriveStiffness(1e-3)
 
-        x = mesh.coor()
-        delta_u = np.zeros_like(x)
+        drive_active = np.zeros((3, 2), dtype=bool)
+        drive_u = np.zeros((3, 2), dtype=float)
 
-        for i in range(delta_u.shape[0]):
-            delta_u[i, 0] = 0.1 * (x[i, 0] - x[0, 0])
+        drive_active[-1, 0] = True
+        drive_u[-1, 0] = 0.1
 
-        system.eventDriven_setDeltaU(delta_u)
+        system.initEventDriven(drive_u, drive_active)
 
         system.eventDrivenStep(1e-4, False)
         self.assertTrue(np.allclose(GMat.Epsd(system.plastic_Eps()), epsy[0, 0] - 0.5 * 1e-4))
