6eba9bf905a7c3759466d1785d4361b5ee2d1f91
Tom de Geus <tom@geus.me>
2022-07-08 11:30:56 +0200

    UniformMultiLayerLeverDrive2d: Merging implementation in header for maintainability

--- include/FrictionQPotFEM/UniformMultiLayerLeverDrive2d.h
+++ include/FrictionQPotFEM/UniformMultiLayerLeverDrive2d.h
@@ -25,7 +25,10 @@ namespace UniformMultiLayerLeverDrive2d {
 /**
 \copydoc Generic2d::version_dependencies()
 */
-inline std::vector<std::string> version_dependencies();
+inline std::vector<std::string> version_dependencies()
+{
+    return Generic2d::version_dependencies();
+}
 
 /**
 Similar to UniformMultiLayerIndividualDrive2d::System(), but with the difference that the
@@ -63,81 +66,10 @@ public:
         const xt::xtensor<size_t, 1>& iip,
         const std::vector<xt::xtensor<size_t, 1>>& elem,
         const std::vector<xt::xtensor<size_t, 1>>& node,
-        const xt::xtensor<bool, 1>& layer_is_plastic);
-
-    std::string type() const override;
-
-    /**
-    Set the lever properties.
-
-    \tparam T e.g. `xt::xtensor<double, 1>`
-    \param H The height of the spring pulling the lever.
-    \param hi The height \f$ h_i \f$ of the loading frame of each layer [nlayer].
-    */
-    template <class T>
-    void setLeverProperties(double H, const T& hi);
-
-    /**
-    Set the target 'position' of the spring pulling the lever.
-
-    \param xdrive Lever position
-    */
-    void setLeverTarget(double xdrive);
-
-    /**
-    Get the current target lever 'position'.
-
-    \return double
-    */
-    double leverTarget() const;
-
-    /**
-    Get the current lever 'position'.
-
-    \return double
-    */
-    double leverPosition() const;
-
-    /**
-    Initialise the event driven protocol by applying a perturbation to loading spring
-    and computing and storing the linear, purely elastic, response.
-    The system can thereafter be moved forward to the next event.
-    Note that this function itself does not change the system in any way,
-    it just stores the relevant perturbations.
-
-    \param xdrive Target 'position' of the spring pulling the lever.
-
-    \param active
-        For each layer and each degree-of-freedom specify if
-        the spring is active (`true`) or not (`false`) [#nlayer, 2].
-    \
-    */
-    template <class T>
-    void initEventDriven(double xdrive, const T& active);
-
-    /**
-    Restore perturbation used from event driven protocol.
-    \param xdrive See eventDriven_leverPosition().
-    \param active See eventDriven_targetActive().
-    \param delta_u See eventDriven_deltaU().
-    \param delta_ubar See eventDriven_deltaUbar().
-    \return Value with which the input perturbation is scaled, see also eventDriven_deltaU().
-    */
-    template <class T, class U, class W>
-    double initEventDriven(double xdrive, const T& active, const U& delta_u, const W& delta_ubar);
-
-    double eventDrivenStep(
-        double deps,
-        bool kick,
-        int direction = +1,
-        bool yield_element = false,
-        bool fallback = false) override;
-
-    /**
-    Get target 'position' of the spring pulling the lever perturbation used for event driven code.
-    \return Value
-    */
-    double eventDriven_deltaLeverPosition() const;
+        const xt::xtensor<bool, 1>& layer_is_plastic)
+    {
+        this->init_lever(coor, conn, dofs, iip, elem, node, layer_is_plastic);
+    }
 
 protected:
     /**
@@ -159,14 +91,251 @@ protected:
         const xt::xtensor<size_t, 1>& iip,
         const std::vector<xt::xtensor<size_t, 1>>& elem,
         const std::vector<xt::xtensor<size_t, 1>>& node,
-        const xt::xtensor<bool, 1>& layer_is_plastic);
+        const xt::xtensor<bool, 1>& layer_is_plastic)
+    {
+        this->init(coor, conn, dofs, iip, elem, node, layer_is_plastic);
 
+        m_lever_hi.resize({m_n_layer});
+        m_lever_hi.fill(0.0);
+
+        m_lever_hi_H.resize({m_n_layer});
+        m_lever_hi_H.fill(0.0);
+
+        m_lever_hi_H_2.resize({m_n_layer});
+        m_lever_hi_H_2.fill(0.0);
+
+        m_lever_H = 0.0;
+        m_lever_target = 0.0;
+        m_lever_u = 0.0;
+    }
+
+public:
+    std::string type() const override
+    {
+        return "FrictionQPotFEM.UniformMultiLayerLeverDrive2d.System";
+    }
+
+    /**
+    Set the lever properties.
+
+    \tparam T e.g. `xt::xtensor<double, 1>`
+    \param H The height of the spring pulling the lever.
+    \param hi The height \f$ h_i \f$ of the loading frame of each layer [nlayer].
+    */
+    template <class T>
+    void setLeverProperties(double H, const T& hi)
+    {
+        FRICTIONQPOTFEM_ASSERT(xt::has_shape(hi, m_lever_hi.shape()));
+        m_lever_set = true;
+        m_lever_hi = hi;
+        m_lever_H = H;
+
+        m_lever_hi_H = m_lever_hi / H;
+        m_lever_hi_H_2 = xt::pow(m_lever_hi / H, 2.0);
+
+        this->updated_u(); // updates forces
+    }
+
+    /**
+    Set the target 'position' of the spring pulling the lever.
+
+    \param xdrive Lever position
+    */
+    void setLeverTarget(double xdrive)
+    {
+        m_lever_target = xdrive;
+        this->updated_u(); // updates target average displacement per layer, and forces
+    }
+
+    /**
+    Get the current target lever 'position'.
+
+    \return double
+    */
+    double leverTarget() const
+    {
+        return m_lever_target;
+    }
+
+    /**
+    Get the current lever 'position'.
+
+    \return double
+    */
+    double leverPosition() const
+    {
+        return m_lever_u;
+    }
+
+    /**
+    Initialise the event driven protocol by applying a perturbation to loading spring
+    and computing and storing the linear, purely elastic, response.
+    The system can thereafter be moved forward to the next event.
+    Note that this function itself does not change the system in any way,
+    it just stores the relevant perturbations.
+
+    \param xlever Target 'position' of the spring pulling the lever.
+
+    \param active
+        For each layer and each degree-of-freedom specify if
+        the spring is active (`true`) or not (`false`) [#nlayer, 2].
+    \
+    */
+    template <class T>
+    void initEventDriven(double xlever, const T& active)
+    {
+        FRICTIONQPOTFEM_ASSERT(this->isHomogeneousElastic());
+        FRICTIONQPOTFEM_ASSERT(xt::has_shape(active, m_layerdrive_active.shape()));
+
+        // backup system
+
+        auto u0 = m_u;
+        auto v0 = m_v;
+        auto a0 = m_a;
+        auto active0 = m_layerdrive_active;
+        auto ubar0 = m_layerdrive_targetubar;
+        auto xdrive0 = m_lever_target;
+        auto t0 = m_t;
+        std::vector<std::vector<xt::xtensor<double, 1>>> epsy0;
+        epsy0.resize(m_nelem_plas);
+        double i = std::numeric_limits<double>::max();
+        xt::xtensor<double, 1> epsy_elas = {-i, i};
+
+        for (size_t e = 0; e < m_nelem_plas; ++e) {
+            epsy0[e].resize(m_nip);
+            for (size_t q = 0; q < m_nip; ++q) {
+                auto& cusp = m_material_plas.refCusp({e, q});
+                epsy0[e][q] = cusp.epsy();
+                cusp.reset_epsy(epsy_elas, false);
+            }
+        }
+
+        // perturbation
+
+        m_u.fill(0.0);
+        m_v.fill(0.0);
+        m_a.fill(0.0);
+        this->updated_u();
+        FRICTIONQPOTFEM_ASSERT(xt::all(xt::equal(this->plastic_CurrentIndex(), 0)));
+        this->layerSetTargetActive(active);
+        this->setLeverTarget(xlever);
+        this->minimise();
+        FRICTIONQPOTFEM_ASSERT(xt::all(xt::equal(this->plastic_CurrentIndex(), 0)));
+        auto up = m_u;
+        m_u.fill(0.0);
+
+        // restore yield strains
+        // to avoid running out-of-bounds there  the displacements had to be reset to zero
+        // if the yield strains are result only later scaling is buggy because a typical yield
+        // strain can be inaccurate
+
+        for (size_t e = 0; e < m_nelem_plas; ++e) {
+            for (size_t q = 0; q < m_nip; ++q) {
+                auto& cusp = m_material_plas.refCusp({e, q});
+                cusp.reset_epsy(epsy0[e][q], false);
+            }
+        }
+
+        // store result
+
+        auto c = this->eventDriven_setDeltaU(up);
+        m_pert_layerdrive_active = active;
+        m_pert_layerdrive_targetubar = c * m_layerdrive_targetubar;
+        m_pert_lever_target = c * xlever;
+
+        // restore system
+
+        this->setU(u0);
+        this->setV(v0);
+        this->setA(a0);
+        this->layerSetTargetActive(active0);
+        this->layerSetTargetUbar(ubar0);
+        this->setLeverTarget(xdrive0);
+        this->setT(t0);
+    }
+
+    /**
+    Restore perturbation used from event driven protocol.
+    \param xlever See eventDriven_leverPosition().
+    \param active See eventDriven_targetActive().
+    \param delta_u See eventDriven_deltaU().
+    \param delta_ubar See eventDriven_deltaUbar().
+    \return Value with which the input perturbation is scaled, see also eventDriven_deltaU().
+    */
+    template <class T, class U, class W>
+    double initEventDriven(double xlever, const T& active, const U& delta_u, const W& delta_ubar)
+    {
+        FRICTIONQPOTFEM_ASSERT(xt::has_shape(delta_ubar, m_layerdrive_targetubar.shape()));
+        FRICTIONQPOTFEM_ASSERT(xt::has_shape(active, m_layerdrive_active.shape()));
+        FRICTIONQPOTFEM_ASSERT(xt::has_shape(delta_u, m_u.shape()));
+        double c = this->eventDriven_setDeltaU(delta_u);
+        m_pert_layerdrive_active = active;
+        m_pert_layerdrive_targetubar = c * delta_ubar;
+        m_pert_lever_target = c * xlever;
+        return c;
+    }
+
+    double eventDrivenStep(
+        double deps,
+        bool kick,
+        int direction = +1,
+        bool yield_element = false,
+        bool fallback = false) override
+    {
+        double c =
+            Generic2d::System::eventDrivenStep(deps, kick, direction, yield_element, fallback);
+        this->layerSetTargetUbar(m_layerdrive_targetubar + c * m_pert_layerdrive_targetubar);
+        this->setLeverTarget(m_lever_target + c * m_pert_lever_target);
+        return c;
+    }
+
+    /**
+    Get target 'position' of the spring pulling the lever perturbation used for event driven code.
+    \return Value
+    */
+    double eventDriven_deltaLeverPosition() const
+    {
+        return m_pert_lever_target;
+    }
+
+protected:
     /**
     Evaluate relevant forces when m_u is updated.
     The assumption is made that the lever has no inertia:
     its position is computed by assuming that the sum of moments acting on it is zero.
     */
-    void updated_u() override;
+    void updated_u() override
+    {
+        FRICTIONQPOTFEM_ASSERT(m_lever_set);
+
+        this->computeLayerUbarActive();
+
+        // Position of the lever based on equilibrium
+
+        m_lever_u = m_lever_target;
+        double n = 1.0;
+
+        for (size_t i = 0; i < m_n_layer; ++i) {
+            if (m_layerdrive_active(i, 0)) {
+                m_lever_u += m_layer_ubar(i, 0) * m_lever_hi_H(i);
+                n += m_lever_hi_H_2(i);
+            }
+        }
+
+        m_lever_u /= n;
+
+        // Update position of driving springs
+
+        for (size_t i = 0; i < m_n_layer; ++i) {
+            if (m_layerdrive_active(i, 0)) {
+                m_layerdrive_targetubar(i, 0) = m_lever_hi_H(i) * m_lever_u;
+            }
+        }
+
+        // Update forces
+        this->computeForceFromTargetUbar();
+        this->computeForceMaterial();
+    }
 
 private:
     bool m_lever_set = false; ///< Lock class until properties have been set.
@@ -181,7 +350,5 @@ private:
 
 } // namespace UniformMultiLayerLeverDrive2d
 } // namespace FrictionQPotFEM
-
-#include "UniformMultiLayerLeverDrive2d.hpp"
 
 #endif
