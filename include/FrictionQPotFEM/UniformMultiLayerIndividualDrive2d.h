215de8ca3be152f5344627461d1f733bb580daf1
Tom de Geus <tom@geus.me>
2022-07-31 15:38:17 +0200

    Fixing tests

--- include/FrictionQPotFEM/UniformMultiLayerIndividualDrive2d.h
+++ include/FrictionQPotFEM/UniformMultiLayerIndividualDrive2d.h
@@ -414,7 +414,7 @@ public:
         FRICTIONQPOTFEM_ASSERT(xt::all(xt::equal(m_plas.i(), 0)));
         this->layerSetTargetActive(active);
         this->layerSetTargetUbar(ubar);
-        this->minimise();
+        this->minimise(0);
         FRICTIONQPOTFEM_ASSERT(xt::all(xt::equal(m_plas.i(), 0)));
         auto up = m_u;
         m_u.fill(0.0);
