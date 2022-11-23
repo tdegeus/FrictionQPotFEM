8ac050ed78d5bd56d38eb23e83c98ed7b07ba4cf
Tom de Geus <tom@geus.me>
2022-11-23 14:48:53 +0100

    Adding long description to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
+from pathlib import Path
+
 from setuptools_scm import get_version
 from skbuild import setup
 
 project_name = "FrictionQPotFEM"
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name=project_name,
     description="Friction model based on GooseFEM and FrictionQPotFEM.",
-    long_description="Friction model based on GooseFEM and FrictionQPotFEM.",
+    long_description=long_description,
     version=get_version(),
     license="MIT",
     author="Tom de Geus",
